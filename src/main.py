d305fd7e67f925cac14e2fa0401559debe6235fa
Antonio Griego <deano505x@gmail.com>
2020-10-28 19:25:39 -0600

    updated main.py with complete player vs player logic
    1. added game logic for determining win condition
    2. updated graphics

--- src/main.py
+++ src/main.py
@@ -1,10 +1,34 @@
 # import the pygame module, so you can use it
 import pygame
+import gamestate
+
+
+# bad boy global variables
+game_state = gamestate.Gamestate()
+
+
+def drawXY(screen, player):
+    pos = pygame.mouse.get_pos()
+    x = int(pos[0]/160)
+    y = int(pos[1] /120)
+    x_pixel = x * 160 + 35
+    y_pixel = y * 120 + 15
+
+    # Debug print statement
+    print('x = ' + str(x) + ', y = ' + str(y))
+
+    if game_state.check_and_update_board(x, y) == True:
+        screen.blit(player, (x_pixel, y_pixel))
+        pygame.display.flip()
+        print(game_state.board)
+
+        if game_state.check_win_condition(x, y) == True:
+            # do stuff to indicate who won!
+            print('TODO: add victory logic here')
 
 
 # define a main function
 def main():
-
     # initialize the pygame module
     pygame.init()
 
@@ -18,11 +42,10 @@ def main():
 
     # paint the gameboard background on screen
     # image set up for unix environment
-
     bgd_image = pygame.image.load("../assets/gameboard_01.png")
+    bgd_image = pygame.transform.scale(bgd_image, (480, 360))
     player_X = pygame.image.load("../assets/player_X.png")
     player_O = pygame.image.load("../assets/player_O.png")
-    bgd_image = pygame.transform.scale(bgd_image, (480, 360))
     screen.fill((255, 255, 255))
     screen.blit(bgd_image, (0, 0))
     pygame.display.flip()
@@ -36,22 +59,13 @@ def main():
         for event in pygame.event.get():
             # mouse click event
             if event.type == pygame.MOUSEBUTTONUP:
-                pos = pygame.mouse.get_pos()
-                x = int(pos[0]/160) * 160 + 35
-                y = int(pos[1] /120) * 120 + 15
-
-                print('x = ' + str(int(pos[0] / 160)) + ', y = ' + str(int(pos[1] / 120)))
-
-                screen.blit(player_O, (x,y))
-                pygame.display.flip()
-
-
+                if game_state.player_Xs_turn == True:
+                    drawXY(screen, player_X)
+                    game_state.player_Xs_turn = False
+                else:
+                    drawXY(screen, player_O)
+                    game_state.player_Xs_turn = True
             # only do something if the event is of type QUIT
-
-
-
-
-
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
