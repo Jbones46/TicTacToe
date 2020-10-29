17651b895ad49016f89d4812a7bca8255da6e4fd
Justin <justin@topratedentmt.com>
2020-10-28 23:23:50 -0400

    Plick to Claymore

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 # import the pygame module, so you can use it
 import pygame
-from tkinter import *
-from tkinter import messagebox
+from pygame.locals import *
 import gamestate
 
 
@@ -11,17 +10,28 @@ def drawXY(screen, player):
     y = int(pos[1] /120)
     x_pixel = x * 160 + 35
     y_pixel = y * 120 + 15
+
     # Debug print statement
     # print('x = ' + str(x) + ', y = ' + str(y))
     if game_state.check_and_update_board(x, y) == True:
         screen.blit(player, (x_pixel, y_pixel))
         pygame.display.flip()
         if game_state.check_win_condition(x, y) == True:
-            Tk().wm_withdraw() #to hide the main window
+            # Tk().wm_withdraw() #to hide the main window
             if game_state.player_Xs_turn == True:
-                messagebox.showinfo('Play Again','Player X wins!')
+                screen.fill((255, 255, 255))
+                screen.blit(win, (20, 20))
+                screen.blit(message, (40, 60))
+                pygame.display.flip()
+
+                # messagebox.showinfo('Play Again','Player X wins!')
             else:
-                messagebox.showinfo('Play Again','Player O wins!')
+                screen.fill((255, 255, 255))
+                screen.blit(lose, (20, 20))
+                screen.blit(message, (40, 60))
+                pygame.display.flip()
+                # messagebox.showinfo('Play Again','Player O wins!')
+
             game_state.reset_game = True
         print()
         game_state.print_game_board()
@@ -30,6 +40,35 @@ def drawXY(screen, player):
 
 # define a main function
 def main():
+    # define a variable to control the main loop
+    running = True
+
+    # main loop
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # mouse click event
+            if event.type == pygame.MOUSEBUTTONUP and game_state.reset_game == False:
+                if game_state.player_Xs_turn == True:
+                    drawXY(screen, player_X)
+                    game_state.player_Xs_turn = False
+                else:
+                    drawXY(screen, player_O)
+                    game_state.player_Xs_turn = True
+            elif event.type == pygame.MOUSEBUTTONUP and game_state.reset_game == True:
+                screen.fill((255, 255, 255))
+                screen.blit(bgd_image, (0, 0))
+                pygame.display.flip()
+                game_state.reset()
+
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+
+
+# run the main function only if this module is executed as the main script
+# (if you import this as a module then nothing is executed)
+if __name__=="__main__":
     # initialize the pygame module
     pygame.init()
 
@@ -47,36 +86,13 @@ def main():
     bgd_image = pygame.transform.scale(bgd_image, (480, 360))
     player_X = pygame.image.load("../assets/player_X.png")
     player_O = pygame.image.load("../assets/player_O.png")
+    screen.fill((255, 255, 255))
+    screen.blit(bgd_image, (0, 0))
+    pygame.display.flip()
 
-    # define a variable to control the main loop
-    running = True
-
-    # main loop
-    while running:
-        if game_state.reset_game == True:
-            screen.fill((255, 255, 255))
-            screen.blit(bgd_image, (0, 0))
-            pygame.display.flip()
-            game_state.reset()
-
-        # event handling, gets all event from the event queue
-        for event in pygame.event.get():
-            # mouse click event
-            if event.type == pygame.MOUSEBUTTONUP:
-                if game_state.player_Xs_turn == True:
-                    drawXY(screen, player_X)
-                    game_state.player_Xs_turn = False
-                else:
-                    drawXY(screen, player_O)
-                    game_state.player_Xs_turn = True
-            # only do something if the event is of type QUIT
-            if event.type == pygame.QUIT:
-                # change the value to False, to exit the main loop
-                running = False
-
-
-# run the main function only if this module is executed as the main script
-# (if you import this as a module then nothing is executed)
-if __name__=="__main__":
+    font = pygame.font.SysFont(None, 48)
+    lose = font.render('Player X LOSES', True, (0, 0, 255))
+    message = font.render('Plick to Claymore', True, (0, 0, 0))
+    win = font.render('Player X WINS', True, (255, 0, 0))
     game_state = gamestate.Gamestate()
     main()
